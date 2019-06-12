c0d5dcbe6dab78abef20d8b0b2d9aaf3c6ee9b55
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 14:17:21 -0700

    feat(orderform): Added nav target for Order Form

---
+++ application/src/components/order-form/OrderForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Template } from '../../components';
+import './OrderForm.css';
+
+class OrderForm extends Component {
+    render() {
+        return (
+            <Template>
+            </Template>
+        );
+    }
+}
+
+export default OrderForm;
