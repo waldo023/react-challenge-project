2673ff769a10962bbd4633ce359a0eb16398c356
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 16:40:49 -0700

    feat(orderform): Added orderform ui

--- application/src/components/order-form/orderForm.js
+++ application/src/components/order-form/orderForm.js
@@ -7,16 +7,47 @@ class OrderForm extends Component {
         super(props);
         this.state = {
             item: "",
-            quantity: 0
+            quantity: 1
         }
+    }
+
+    menuItemChosen(event) {
+        this.setState({ item: event.target.value });
+    }
+
+    menuQuantityChosen(event) {
+        this.setState({ quantity: event.target.value });
     }
 
     render() {
         return (
             <Template>
-                <form>
-
-                </form>
+                <div className="form-wrapper">
+                    <form>
+                        <label className="form-label">I'd like to order...</label><br />
+                        <select 
+                            value={this.state.item} 
+                            onChange={(event) => this.menuItemChosen(event)}
+                            className="menu-select"
+                        >
+                            <option value="" defaultValue disabled hidden>Lunch menu</option>
+                            <option value="Soup of the Day">Soup of the Day</option>
+                            <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
+                            <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
+                            <option value="Chili Con Carne">Chili Con Carne</option>
+                        </select><br />
+                        <label className="qty-label">Qty:</label>
+                        <select value={this.state.quantity} onChange={(event) => this.menuQuantityChosen(event)}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                        </select>
+                        <button type="button" className="order-btn">Order It!</button>
+                    </form>
+                </div>
             </Template>
         );
     }
