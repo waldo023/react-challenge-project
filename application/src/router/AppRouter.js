455c50284b6cdd6f53f151790868162a815cd324
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 14:17:51 -0700

    feat(router): Linked new nav targets in the router

--- application/src/router/AppRouter.js
+++ application/src/router/AppRouter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login } from '../components';
+import { Main, Login, OrderForm, ViewOrders } from '../components';
 
 const AppRouter = (props) => {
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
+      <Route path="/order" exact component={OrderForm} />
+      <Route path="/view-orders" exact component={ViewOrders} />
     </Router>
   );
 }
