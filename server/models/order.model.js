7eb2fc4f49b784a67f5731d9f910a1ef1385df86
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-12 12:45:26 -0700

    refac(order model): added ordered by

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const OrderSchema = new mongoose.Schema({
   order_item: String,
   quantity: Number,
+  orderedBy: String,
 }, {
   timestamps: true,
   collection: 'Orders',
