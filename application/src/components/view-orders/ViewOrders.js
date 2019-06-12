b4eb8376ee41debab3fec24332676143df5e206c
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 14:17:38 -0700

    feat(vieworders): Added nav target for view orders

---
+++ application/src/components/view-orders/ViewOrders.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Template } from '../../components';
+import './ViewOrders.css';
+
+class ViewOrders extends Component {
+    render() {
+        return (
+            <Template>
+            </Template>
+        );
+    }
+}
+
+export default ViewOrders;
