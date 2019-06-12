c2f329894780df8856e09a6b89e447b86f464d51
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 14:18:04 -0700

    chore(export): Exported local modules

--- application/src/components/index.js
+++ application/src/components/index.js
@@ -1,4 +1,8 @@
-import Main from './main/Main';
 import Login from './login/Login';
+import Main from './main/Main';
+import Nav from './nav/Nav';
+import OrderForm from './order-form/OrderForm';
+import Template from './common/Template';
+import ViewOrders from './view-orders/ViewOrders';
 
-export { Main, Login };
+export { Login, OrderForm, Main, Nav, Template, ViewOrders };
