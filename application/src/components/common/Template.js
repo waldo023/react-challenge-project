62d374592c39ee95a1d599c1065147485902f1bc
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2019-06-12 14:15:52 -0700

    feat(template): App template

---
+++ application/src/components/common/Template.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Nav } from '../../components';
+import './Template.css';
+
+class Template extends Component {
+    render() {
+        return (
+            <div className="bg-layer">
+                <div className="fg-layer">
+                    <label className="logo">Bruce's Diner</label>
+                    <Nav />
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Template;
