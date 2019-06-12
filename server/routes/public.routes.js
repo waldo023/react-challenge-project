16fe38f6eecbc99e3c34efffc7d6faf27fc78d9f
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-12 10:14:25 -0700

    feat(routes): get orders

--- server/routes/public.routes.js
+++ server/routes/public.routes.js
@@ -8,34 +8,46 @@ router.get('/test', (req, res) => {
   res.json({ success: true });
 });
 
+router.get('/current-orders', async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json({ success: true, orders });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+});
+
 router.post('/add-order', async (req, res) => {
-  if (!req.body) {
-    res.status(400).json({ success: false, error: 'No information sent.' })
-    return;
+  try {
+    if (!req.body) {
+      res.status(400).json({ success: false, error: 'No information sent.' })
+      return;
+    }
+  
+    if (!req.body.order_item) {
+      res.status(400).json({ success: false, error: 'No order item sent.'});
+      return;
+    }
+  
+    if (!req.body.quantity) {
+      res.status(400).json({ success: false, error: 'No quantity sent.'})
+      return;
+    }
+  
+    const orderObj = new Order({
+      order_item: req.body.order_item,
+      quantity: req.body.quantity
+    });
+  
+    const dbResponse = await orderObj.save();
+    if (dbResponse && dbResponse._id) {
+      res.status(200).json({ success: true, insertedId: dbResponse._id });
+    } else {
+      res.status(400).json({ success: false, error: 'Database Error' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error });
   }
-
-  if (!req.body.order_item) {
-    res.status(400).json({ success: false, error: 'No order item sent.'});
-    return;
-  }
-
-  if (!req.body.quantity) {
-    res.status(400).json({ success: false, error: 'No quantity sent.'})
-    return;
-  }
-
-  const orderObj = new Order({
-    order_item: req.body.order_item,
-    quantity: req.body.quantity
-  });
-
-  const dbResponse = await orderObj.save();
-  if (dbResponse && dbResponse._id) {
-    res.status(200).json({ success: true, insertedId: dbResponse._id });
-  } else {
-    res.status(400).json({ success: false, error: 'Database Error' });
-  }
-
 });
 
 module.exports = router;
