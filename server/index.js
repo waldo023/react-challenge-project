16b0435c52df554e9a1180d489ef579f1d53a615
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-12 09:25:07 -0700

    feat(server index): connect to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 4000;
+
+require('dotenv').config();
+
+// DATABASE SETUP
+// connect to db
+mongoose.connect(process.env.MONGODATABASE);
+// display message on connect
+mongoose.connection.on('connected', () => {
+  console.log('Connected to databse: ', process.env.MONGODATABASE);
+});
+// display message on error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: ', err);
+});
 
 app.get('/', (req, res) => {
     console.log('Hi!');
