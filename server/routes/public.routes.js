cfce7fe8c621cefeace61b248f6727d64f4b7943
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-12 10:05:52 -0700

    feat(routes): add order route

--- server/routes/public.routes.js
+++ server/routes/public.routes.js
@@ -8,4 +8,34 @@ router.get('/test', (req, res) => {
   res.json({ success: true });
 });
 
+router.post('/add-order', async (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ success: false, error: 'No information sent.' })
+    return;
+  }
+
+  if (!req.body.order_item) {
+    res.status(400).json({ success: false, error: 'No order item sent.'});
+    return;
+  }
+
+  if (!req.body.quantity) {
+    res.status(400).json({ success: false, error: 'No quantity sent.'})
+    return;
+  }
+
+  const orderObj = new Order({
+    order_item: req.body.order_item,
+    quantity: req.body.quantity
+  });
+
+  const dbResponse = await orderObj.save();
+  if (dbResponse && dbResponse._id) {
+    res.status(200).json({ success: true, insertedId: dbResponse._id });
+  } else {
+    res.status(400).json({ success: false, error: 'Database Error' });
+  }
+
+});
+
 module.exports = router;
