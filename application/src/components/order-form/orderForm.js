f8a2a4df1bd9fd30b5f9bb6cf30feef8421c4b79
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-06-22 10:51:58 -0700

    fix(orderform): intentional bug fix

--- application/src/components/order-form/orderForm.js
+++ application/src/components/order-form/orderForm.js
@@ -20,7 +20,7 @@ class OrderForm extends Component {
     }
 
     menuItemChosen(event) {
-        this.setState({ item: event.target.value });
+        this.setState({ order_item: event.target.value });
     }
 
     menuQuantityChosen(event) {
