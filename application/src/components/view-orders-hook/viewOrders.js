4835fd42e017293e9ad3e784bce5de21862e77ab
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-06-23 10:31:12 -0700

    fix(view orders): proper import

--- application/src/components/view-orders-hook/viewOrders.js
+++ application/src/components/view-orders-hook/viewOrders.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
 import './viewOrders.css';
