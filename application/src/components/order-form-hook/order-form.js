8ee5c79d842c7d2021c63c746eb445c2b427c629
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-02 11:12:55 -0700

    style(order form - hook): indentation

--- application/src/components/order-form-hook/order-form.js
+++ application/src/components/order-form-hook/order-form.js
@@ -36,32 +36,32 @@ export default function OrderForm(props) {
 
     return (
         <Template>
-                <div className="form-wrapper">
-                    <form>
-                        <label className="form-label">I'd like to order...</label><br />
-                        <select 
-                            value={orderItem} 
-                            onChange={(event) => menuItemChosen(event)}
-                            className="menu-select"
-                        >
-                            <option value="" defaultValue disabled hidden>Lunch menu</option>
-                            <option value="Soup of the Day">Soup of the Day</option>
-                            <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
-                            <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
-                            <option value="Chili Con Carne">Chili Con Carne</option>
-                        </select><br />
-                        <label className="qty-label">Qty:</label>
-                        <select value={quantity} onChange={(event) => menuQuantityChosen(event)}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                        </select>
-                        <button type="button" className="order-btn" onClick={(event) => submitOrder(event)}>Order It!</button>
-                    </form>
-                </div>
-            </Template>
+            <div className="form-wrapper">
+                <form>
+                    <label className="form-label">I'd like to order...</label><br />
+                    <select 
+                        value={orderItem} 
+                        onChange={(event) => menuItemChosen(event)}
+                        className="menu-select"
+                    >
+                        <option value="" defaultValue disabled hidden>Lunch menu</option>
+                        <option value="Soup of the Day">Soup of the Day</option>
+                        <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
+                        <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
+                        <option value="Chili Con Carne">Chili Con Carne</option>
+                    </select><br />
+                    <label className="qty-label">Qty:</label>
+                    <select value={quantity} onChange={(event) => menuQuantityChosen(event)}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select>
+                    <button type="button" className="order-btn" onClick={(event) => submitOrder(event)}>Order It!</button>
+                </form>
+            </div>
+        </Template>
     )
 }
